85fb79854df5eeaa93e9f821eaa5c71a547ac92f
Hek.sh <301932612+hek-sh@users.noreply.github.com>
2026-07-21 00:18:36 -0600

    feat: initial configuration files for Hyprland setup

---
+++ ui/.config/hypr/hyprland.lua
@@ -0,0 +1,13 @@
+require("modules.autostart")
+require("modules.binds")
+require("modules.env")
+require("modules.input")
+require("modules.misc")
+require("modules.monitors")
+require("modules.permissions")
+require("modules.ui")
+require("modules.workspaces")
+
+
+
+
