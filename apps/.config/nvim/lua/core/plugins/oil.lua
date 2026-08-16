6ed5fbeae256fa61aa21e1393315cafc83727c25
Hek <301932612+hek-sh@users.noreply.github.com>
2026-08-15 22:40:39 -0600

    feat: add view options for oil.nvim plugin configuration

--- apps/.config/nvim/lua/core/plugins/oil.lua
+++ apps/.config/nvim/lua/core/plugins/oil.lua
@@ -2,7 +2,23 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    view_options = {
+      show_hidden = true,
+      is_hidden_file = function(name, bufnr)
+        return vim.startswith(name, ".")
+      end,
+      is_always_hidden = function(name, bufnr)
+        return name == ".." or name == ".git"
+      end,
+      natural_order = true,
+      case_insensitive = false,
+      sort = {
+        { "type", "asc" },
+        { "name", "asc" },
+      },
+    },
+  },
   -- Optional dependencies
   dependencies = { { "nvim-mini/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
