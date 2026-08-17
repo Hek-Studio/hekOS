d8662657fcfa586801a27e3b4263785c3365b86b
Hek <301932612+hek-sh@users.noreply.github.com>
2026-08-17 12:26:11 -0600

    feat: install and autostart hyprpolkitagent for improved authentication handling

--- install/01-base-packages.sh
+++ install/01-base-packages.sh
@@ -6,9 +6,9 @@ print_info "Installing essential system packages..."
 retry_command sudo pacman -S --needed --noconfirm \
   stow git base-devel \
   waybar rofi swaync wlogout chromium bluetui pavucontrol \
-  hyprland kitty hypridle hyprlock brightnessctl playerctl \
+  hyprland kitty hypridle hyprlock brightnessctl playerctl hyprpolkitagent \
   yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick mpv imv \
-  xdg-desktop-portal-hyprland nwg-look gnome-themes-extra \
+  xdg-desktop-portal-hyprland nwg-look gnome-themes-extra\
   qt5-wayland qt6-wayland \
   gcc make ripgrep fd tree-sitter-cli unzip neovim lazygit \
   wl-clipboard grim slurp \
